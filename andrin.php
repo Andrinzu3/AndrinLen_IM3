ab191a4db083ae178eefb00b15fd6abfd71a4738
Andrin Zuend <andrin.zuend@stud.fhgr.ch>
2025-10-01 11:48:09 +0200

    az
    az

--- andrin.php
+++ andrin.php
@@ -1,5 +1,6 @@
 <?php
 
+// -> Variable definieren
 
 $name = "Andrin";
 echo $name;
@@ -9,3 +10,34 @@ echo '<br';
 $a = 292;
 $b = 22;
 echo $a + $b;
+
+?>
+
+// -> funktionen
+
+<h1> Hallo <?php echo $name ?> </h1>
+
+<?php
+// Funktion definieren
+function multiply($a, $b) {
+    return $a * $b;
+}
+
+// Funktion aufrufen
+echo multiply(5, 3);  // Ausgabe: 15
+?>
+
+// -> Bedingungen
+
+<?php
+$note = 3.75;
+
+if ($note >= 4) {
+    echo "Bestanden";
+} elseif ($note >= 3.5) {
+    echo "Nachprüfung";
+} else {
+    echo "Nicht bestanden";
+}
+?>
+
