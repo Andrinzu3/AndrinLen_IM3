aff9152835611737cb07c4b6b92c1673779cc9dc
yxllowsquirtle <wilpertlen@gmail.com>
2025-10-01 10:01:32 +0200

    king
    king

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
 document.getElementById('year').textContent = new Date().getFullYear();
-const point = document.getElementById('blinkPoint');
-
-setInterval(() => {
-  point.classList.toggle('hidden');
-}, 600); // alle 600ms sichtbar/unsichtbar
+// Klick auf die Karte -> gibt dir Position in % zurück
+document.querySelector('.map img').addEventListener('click', (e) => {
+    const rect = e.target.getBoundingClientRect();
+    const x = ((e.clientX - rect.left) / rect.width) * 100;
+    const y = ((e.clientY - rect.top) / rect.height) * 100;
+    console.log(`left: ${x.toFixed(1)}%; top: ${y.toFixed(1)}%`);
+  });
