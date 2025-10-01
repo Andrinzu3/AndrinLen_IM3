701f6f940839c75fa836065c1f811b3a7d7e92f3
Andrin Zuend <andrin.zuend@stud.fhgr.ch>
2025-10-01 22:51:50 +0200

    az
    az

--- insert.php
+++ insert.php
@@ -12,20 +12,20 @@ try {
     $pdo = new PDO($dsn, $username, $password, $options);
 
     // SQL-Query mit Platzhaltern für das Einfügen von Daten
-    $sql = "INSERT INTO User (location, firstname, lastname, email) VALUES (?, ?, ?)";
+    $sql = "INSERT INTO User (firstname, lastname, email) VALUES (?, ?, ?)";
 
     // Bereitet die SQL-Anweisung vor
     $stmt = $pdo->prepare($sql);
 
     // Fügt jedes Element im Array in die Datenbank ein
-    foreach ($dataArray as $item) {
+    
         $stmt->execute([
             $data['firstname'],
             $data['lastname'],
             $data['email']
            
         ]);
-    }
+
 
     echo "Daten erfolgreich eingefügt.";
 } catch (PDOException $e) {
