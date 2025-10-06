f439df3518f2f783bcc6b79c486afd3e77592f85
Andrin Zuend <andrin.zuend@stud.fhgr.ch>
2025-10-06 10:26:52 +0200

    az
    az

--- IM3 Übungen/etl-boilerplate/extract.php
+++ IM3 Übungen/etl-boilerplate/extract.php
@@ -16,21 +16,25 @@
 
 function fetchWeatherData()
 {
-    $url = "https://api.open-meteo.com/v1/forecast?latitude=46.9481,46.8499,47.3667&longitude=7.4474,9.5329,8.55&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,rain,showers,snowfall,cloud_cover&temperature_unit=fahrenheit&timezone=auto&forecast_days=1";
+    $url = "https://air-quality-api.open-meteo.com/v1/air-quality?latitude=28.6214&longitude=77.2148&hourly=us_aqi,ozone,carbon_monoxide,carbon_dioxide,uv_index&ref=freepublicapis.com"; 
 
     // Initialisiert eine cURL-Sitzung
-
+    $ch = curl_init($url);
 
     // Setzt Optionen
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 
     // Führt die cURL-Sitzung aus und erhält den Inhalt
+    $response = curl_exec($ch);
 
 
     // Schließt die cURL-Sitzung
+    curl_close($ch);
 
 
     // Dekodiert die JSON-Antwort und gibt Daten zurück
+    return json_decode($response, true);
 
 }
 
